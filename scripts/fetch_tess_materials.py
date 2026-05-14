ab42f552e0b40cc863ebb18ce87fef0001e78fd7
Finn Bacall <fbacall@users.noreply.github.com>
2026-05-14 11:18:01 +0100

    Update fetch_tess_materials.py

--- scripts/fetch_tess_materials.py
+++ scripts/fetch_tess_materials.py
@@ -4,6 +4,14 @@ from pathlib import Path
 import requests
 import sys
 
+url = "https://workflowhub.eu/workflows"
+headers = {
+    "Accept": "application/vnd.api+json",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
+}
+response = requests.get(url, headers=headers)
+print(f"Status: {response.status_code}")
+
 url = "https://tess.elixir-europe.org/materials"
 headers = {
     "Accept": "application/vnd.api+json",
